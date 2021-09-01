c9fdc85719931e551e962cc3149fa550ebd06586
Phạm Trung Hiếu <34348907+Rohupt@users.noreply.github.com>
2021-09-01 11:21:57 +0700

    hotfix

--- database/migrations/2021_08_25_085609_create_book_author_table.php
+++ database/migrations/2021_08_25_085609_create_book_author_table.php
@@ -16,7 +16,7 @@ class CreateBookAuthorTable extends Migration
         Schema::create('book_author', function (Blueprint $table) {
             $table->foreignId('book_id')->constrained('books')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('author_id')->constrained('authors')->onUpdate('cascade')->onDelete('cascade');
-            $table->primary(['book_id', 'authors_id']);
+            $table->primary(['book_id', 'author_id']);
             $table->timestamps();
         });
     }
